4d076954defb01692b345e40cbc48b0b0158c292
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-04-03 01:26:42 +0530

    A quick dirty version of chowk customized for forwarding SMS to PlaySMS

---
+++ playsms_chowk.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from utils import send_to_playsms 
+app = Flask(__name__)
+
+import logging
+from logging import DEBUG, handlers
+
+rfh = handlers.RotatingFileHandler('./chowk.log')
+rfh.setLevel(DEBUG)
+
+#Now, add handlers to all loggers
+loggers = [app.logger, logging.getLogger('utils')]
+
+for l in loggers:
+    l.addHandler(rfh)
+
+@app.route('/receivesms/')
+def receivesms():
+    '''get the sms, put in a URL proper for playSMS and forward it'''
+    
+    try:
+        msg = {}
+        msg['from'] = request.args['from']
+        msg['text'] = request.args['text']
+        msg['args'] = request.args
+        
+        send_to_playsms(msg)
+
+        return ('',200,[])
+    except Exception as e:
+        app.logger.debug("Exception occurred!")
+        raise e
+
+if __name__ == "__main__":
+       app.run(debug = True, host = '0.0.0.0')
---
+++ utils.py
@@ -0,0 +1,38 @@
+from requests import get
+from settings import username, webservices_token, smsc, recvnum, output_format, app, url
+import logging
+#configure our logger
+logger = logging.getLogger(__name__) #thus, utils.logger 
+
+def send_to_playsms(msg = {}):
+    '''sends a given message to the RapidPro server
+    Parameters  Name or description
+    Operation   inject
+    Mandatory   u h from msg recvnum smsc
+    Optional    format
+    Returns     return codes
+    '''
+
+    try:
+        
+        data = { #the data to be sent in the body of the request
+                'app'       : app,
+                'from'      : msg['from'],
+                'msg'       : msg['text'],
+                'recvnum'   : recvnum,
+                'smsc'      : smsc,
+                'u'         : username,
+                'h'         : webservices_token,
+                'op'        : 'inject',
+        }
+
+        r = get(url=url, params = data)
+        logger.debug("Sending request to RapidPro server at %s", r.url)
+        logger.debug("Data inside request to RapidPro server is %s", data)
+        logger.debug("The response we got from RapidPro is %s", r.text)
+
+        r.raise_for_status() #Will raise an exception with the HTTP code ONLY IF the HTTP status was NOT 200
+        return True
+    except Exception as e:
+        logger.debug("Exception %s occurred", e)
+        raise e
